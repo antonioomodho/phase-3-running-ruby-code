ff47d6d7d3eecb8bb09954341dc788354ea5e345
antonioomodho <antonioomodho@gmail.com>
2023-05-17 10:12:04 +0300

    completed lab

--- app.rb
+++ app.rb
@@ -4,9 +4,11 @@
 #     { id: 4, hello: "Learner" }]
 
 # p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+puts "Hello World!"
+puts "Hello world!"
+puts "Hello world!"
+print "Pass this test, please."
+p [1,2,3]
 
 # def add
     
@@ -14,10 +16,9 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+#   puts num1
+#   puts num2
+#   num1 + num2
+#end
 
-puts add(1,3) + add(2,2)
+#puts add(1,3) + add(2,2)
